abe2579fd88e07becf096c172d08298a8f945dac
David Weisgerber <david@fourconnected.de>
2013-02-24 14:35:12 +0100

    Bitrateneinstellungen so modifiziert, dass die Bitrate konstant ist und einen hoffentlich vernünftigen Puffer eingeführt

--- FEG Live Producer/broadcastmanager.cpp
+++ FEG Live Producer/broadcastmanager.cpp
@@ -36,7 +36,7 @@ void BroadcastManager::broadcastClicked()
 	}
 	else
 	{
-		m_casparCon->sendCommand("ADD 1-10 FILE rtmp://localhost/flvplayback/high -f flv -vcodec h264 -acodec aac -r 25 -maxrate 3000000 -b 3000000 -ac 1");
+		m_casparCon->sendCommand("ADD 1-10 FILE rtmp://localhost/flvplayback/high -f flv -vcodec h264 -r 25 -b 3000000 -minrate 3000000 -maxrate 3000000 -bufsize 9000000 -acodec aac -ac 1");
 		m_startBroadcast = QDateTime::currentDateTime();
 		m_isBroadcasting = true;
 	}
