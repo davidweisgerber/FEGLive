0c7eb38bf1157b81f5bd9778ddc3a3431dddcbf8
David Weisgerber <david@fourconnected.de>
2013-03-15 16:11:23 +0100

    Zwei Streams

--- FEG Live Producer/broadcastmanager.cpp
+++ FEG Live Producer/broadcastmanager.cpp
@@ -31,12 +31,14 @@ void BroadcastManager::broadcastClicked()
 	if (m_isBroadcasting)
 	{
 		m_casparCon->sendCommand("REMOVE 1-10");
+		m_casparCon->sendCommand("REMOVE 1-11");
 		m_isBroadcasting = false;
 		m_startBroadcast = QDateTime::currentDateTime();
 	}
 	else
 	{
-		m_casparCon->sendCommand("ADD 1-10 FILE rtmp://localhost/flvplayback/high -f flv -vcodec h264 -r 25 -b 3000000 -minrate 3000000 -maxrate 3000000 -bufsize 9000000 -acodec aac -ac 1");
+		m_casparCon->sendCommand("ADD 1-10 FILE rtmp://localhost/flvplayback/high -f flv -vcodec h264 -r 25 -b 3000000 -minrate 3000000 -maxrate 3000000 -bufsize 18000000 -acodec aac -ac 1");
+		m_casparCon->sendCommand("ADD 1-11 FILE rtmp://localhost/flvplayback/low -f flv -vcodec h264 -s 640x360 -r 25 -b 500000 -minrate 500000 -maxrate 500000 -bufsize 2000000 -acodec aac -ac 1");
 		m_startBroadcast = QDateTime::currentDateTime();
 		m_isBroadcasting = true;
 	}
